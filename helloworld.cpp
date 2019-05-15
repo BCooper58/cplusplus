b3ed6d0e5fd4da1183208712e8ce8253e7840015
BCooper58 <brc51101@gmail.com>
2019-05-15 08:40:41 -0700

    kek

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	cout<<"ABCDEFGHIJKLMNOPQRSTUVWXYZ"<<endl;
+	cout<<"StevePi"<<endl;
+	cout<<"ZULU"<<endl;
+	for (int i ; i < 256 ; 1++){
+		cout<<(char)i;
+	}
+	return 0;
+}
